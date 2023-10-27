a1c51a5e2b44062c0ca284cdbf43d80f6981f56d
Mate <mate@localhost.hu>
2023-10-27 09:30:18 +0200

    Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let input = document.getElementById('addToList');
+let lista = document.getElementById('lista');
+let add = document.getElementById('add');
+let listArray = [];
+
+add.addEventListener('click', function () {
+    let value = input.value;
+    console.log("value: "+value)
+    let li = document.createElement('li');
+    li.textContent = value;
+    let listItems = lista.getElementsByTagName('li');
+    for (let i = 0; i < listItems.length; i++) {
+        listArray.push(listItems[i].innerHTML);
+    }
+    if (listArray.includes(value)) {
+        alert("Ez az item már létezik!");
+    } else {
+        lista.appendChild(li);
+        input.value = "";
+    }
+});
